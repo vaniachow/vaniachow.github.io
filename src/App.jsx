3c6ee46449d909bbb2913a50c0ebb6544340a510
vaniachow <vcbee18@gmail.com>
2023-07-23 09:34:45 -0700

    Added analytics using @vercel/analytics/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar />
-        <Hero />
-      </div>
-      <Works />
-      <Experience />
-      <Tech />
-      <div className="relative z-0">
-      </div>
-    </div>
-    </BrowserRouter>
-  )
+    <Analytics id="prj_TJcrk6KNyP8EqhNJ1zV1Ceq4hUeB">
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <Works />
+          <Experience />
+          <Tech />
+          <div className="relative z-0">
+          </div>
+        </div>
+      </BrowserRouter>
+    </Analytics>
+  );
 }
 
-export default App
+export default App;
